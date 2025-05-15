216121cfdff0a23645e0584392e6ef0fc3bce060
SmyslovAG <a.smyslov@avia.mecom.ru>
2025-05-15 16:20:58 +0300

    build: добавлена автоматизация и плагины в систему сборки gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,89 @@
-const gulp = require('gulp')
-const pug = require('gulp-pug')
-const stylus = require('gulp-stylus')
-const babel = require('gulp-babel')
-const plumber = require('gulp-plumber')
-const notify = require('gulp-notify')
+const autoprefixer = require('autoprefixer') // импортируем autoprefixer как плагин PostCSS
+const babel        = require('gulp-babel')
+const cleanCSS     = require('gulp-clean-css')
+const gulp         = require('gulp')
+const htmlmin      = require('gulp-htmlmin')
+const notify       = require('gulp-notify')
+const plumber      = require('gulp-plumber')
+const postcss      = require('gulp-postcss')
+const pug          = require('gulp-pug')
+const stylus       = require('gulp-stylus')
+const terser       = require('gulp-terser')
 
 const paths = {
-  pug: 'src/amtk_form.pug',
+  build:  'build',
+  js:     'src/amtk_form.js',
+  pug:    'src/amtk_form.pug',
   stylus: 'src/amtk_form.styl',
-  js: 'src/amtk_form.js',
-  build: 'build'
 }
 
-// компиляция Pug в HTML
+// Компиляция Pug в HTML + минификация
 function compilePug() {
   return gulp.src(paths.pug)
-    .pipe(plumber({ errorHandler: notify.onError("Pug Error: <%= error.message %>")}))
-    .pipe(pug({ pretty: true }))
+    .pipe(plumber({
+      errorHandler: notify.onError("Pug Error: <%= error.message %>")
+    }))
+    .pipe(pug({ pretty: false })) // компактный HTML
+    .pipe(htmlmin({
+      collapseWhitespace:        true,
+      minifyCSS:                 true,
+      minifyJS:                  true,
+      removeComments:            true,
+      removeEmptyAttributes:     true,
+      removeRedundantAttributes: true,
+    }))
     .pipe(gulp.dest(paths.build))
 }
 
-// компиляция Stylus в HTML
+// Компиляция Stylus в CSS + автопрефиксы + минификация
 function compileStylus() {
   return gulp.src(paths.stylus)
-    .pipe(plumber({ errorHandler: notify.onError("Stylus Error: <%= error.message %>")}))
+    .pipe(plumber({
+      errorHandler: notify.onError("Stylus Error: <%= error.message %>")
+    }))
     .pipe(stylus())
+    .pipe(postcss([
+      autoprefixer({
+        cascade: false,
+        overrideBrowserslist: [
+          '> 0.5%',
+          'last 2 versions',
+          'Firefox ESR',
+          'not dead',
+        ]
+      })
+    ]))
+    .pipe(cleanCSS({ level: 2 }))
     .pipe(gulp.dest(paths.build))
 }
 
-// компиляция JavaScript в Babel
+// Компиляция JavaScript в Babel + минификация
 function compileJS() {
   return gulp.src(paths.js)
-    .pipe(plumber({ errorHandler: notify.onError("Babel Error: <%= error.message %>")}))
+    .pipe(plumber({
+      errorHandler: notify.onError("Babel Error: <%= error.message %>")
+    }))
     .pipe(babel())
-    .pipe(gulp.dest(paths.build))
+    .pipe(terser())
+    .pipe(gulp.dest(paths.build));
 }
 
-// Общая задачи сборки
-const build = gulp.series(gulp.parallel(compilePug, compileStylus, compileJS))
+// Задача watch - следит за файлами и запускает сборку нужных частей
+function watch() {
+  gulp.watch(paths.pug,    compilePug)
+  gulp.watch(paths.stylus, compileStylus)
+  gulp.watch(paths.js,     compileJS)
+}
 
-exports.build = build
+// Общая задача сборки
+const build = gulp.series(
+  gulp.parallel(
+    compilePug,
+    compileStylus,
+    compileJS
+  )
+)
+
+exports.watch   = watch
+exports.build   = build
 exports.default = build
