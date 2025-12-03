29254872866dc4ab1edb602327219b0406ecdad2
SmyslovAG <a.smyslov@avia.mecom.ru>
2025-12-03 17:54:12 +0300

    wip

--- src/components/metar/main/main.mjs
+++ src/components/metar/main/main.mjs
@@ -3,7 +3,25 @@
 
 export default function(form) {
 
-  console.log(form)
+  const { output } = form
+  const { send, submit, reset } = form.forms.output.elements
+  const send_wrapper = send.parentElement
+  const submit_wrapper = submit.parentElement
+
+  send_wrapper.hidden = false
+  submit_wrapper.hidden = true
+
+  send.addEventListener('click', (e) => {
+    e.preventDefault()
+    send_wrapper.hidden = true
+    submit_wrapper.hidden = false
+  })
+
+  reset.addEventListener('click', (e) => {
+    e.preventDefault()
+    send_wrapper.hidden = false
+    submit_wrapper.hidden = true
+  })
 
   form.forms.output.addEventListener('submit', (e) => {
     e.preventDefault()
